85249f02f2e5154adc0d1a1e68263f9b81527d6c
Alexandr Yurkevich <jrkevichaa20@gmail.com>
2024-03-27 01:18:43 +0300

    circle,fetching places, tabs

--- src/pages/MapPage/Map.js
+++ src/pages/MapPage/Map.js
@@ -1,7 +1,8 @@
-import { TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import { TileLayer, Marker, Popup, useMapEvents, Polyline, Circle, ZoomControl } from "react-leaflet";
 import { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
 import L from 'leaflet';
+import 'leaflet-routing-machine';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
@@ -16,9 +17,19 @@ const DefaultIcon = L.icon({
   shadowSize: [24, 41],
 });
 
-const Map = () => {
-	const [markers,setMarkers] = useState([])
+export default function Map({places,setPlaces,radius}) {
   const [center,setCenter] = useState([0,0])
+  const [route,setRoute] = useState(null)
+
+  function fetchPlacesWithinRadius(center, radius) {
+    const url = `https://overpass-api.de/api/interpreter?data=[out:json];node(around:${radius},${center[0]},${center[1]})["amenity"];out;`;
+  
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => data.elements.map(el=> ({...el, position:[el.lat,el.lon]})))//({id:el.id, tags: el.tags, position:[el.lat,el.lon]})))
+      .catch(error => console.error(error));
+  }
+
   const map = useMapEvents({
     dblclick: () => {
       map.locate()
@@ -28,6 +39,22 @@ const Map = () => {
       const userLoaction = [location.latitude, location.longitude];
       setCenter(userLoaction);
       map.flyTo(userLoaction,17, {animate: false})
+      fetchPlacesWithinRadius(userLoaction, radius)
+        .then((data) => { 
+          setPlaces(data);
+          console.log(data);
+          const router = L.Routing.control({
+            waypoints: [
+              L.latLng(location.latitude, location.longitude),
+              L.latLng(data[7].latitude, data[7].longitude),
+            ],
+          }).addTo(map);
+
+          router.on('routesfound', (event) => {
+            console.log("router - " + event.routes[0].coordinates)
+            setRoute(event.routes[0].coordinates);
+          });
+        })
     }
   })
 
@@ -41,12 +68,17 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         url={TILELAYER_URL}
       />
+      <ZoomControl position="bottomright" />
 			<Marker position={center} icon={DefaultIcon}>
 				<Popup>You're here.</Popup>
+        <Circle
+          center={center}
+          pathOptions={{ color: 'red', fillColor: 'red', fillOpacity: 0.2, weight: 3, dashArray: '5, 10' }}
+          radius={radius}
+        />
 			</Marker>
-      {markers.map((marker) => <Marker position={marker.position}/>)}
+      {places.map((marker) => <Marker key={marker.id} position={marker.position} icon={DefaultIcon}/>)}
+      {route && <Polyline pathOptions={{ color: 'blue' }} positions={route} />}
 		</>
 	);
 };
-
-export default Map;
--- src/pages/MapPage/MapPage.js
+++ src/pages/MapPage/MapPage.js
@@ -1,19 +1,133 @@
-import React from 'react';
+import { Button, Card, IconButton, InputBase, Paper, SvgIcon, Typography } from '@mui/material';
+import LoginIcon from '@mui/icons-material/Login';
+import SearchIcon from '@mui/icons-material/Search';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import CloseIcon from '@mui/icons-material/Close';
+import React, { useState } from 'react';
 import { MapContainer } from 'react-leaflet';
 import Map from './Map';
+import './styles.css'
+import { Link } from 'react-router-dom';
 
-const MapComponent = () => {
+const LogoIcon = ({ primaryColor, secondaryColor, ...props }) => (
+    <SvgIcon {...props}>
+      <path
+        d="M167.49 83.44C167.49 102.57 161.02 120.22 150.13 134.31L132.91 103.82C135.54 97.54 136.99 90.66 136.99 83.44C136.99 56.67 117.03 34.48 91.2003 30.99C88.8604 30.66 86.4704 30.5 84.0504 30.5C81.6304 30.5 79.2504 30.66 76.9203 30.98C51.0903 34.47 31.1104 56.67 31.1104 83.44C31.1104 90.66 32.5603 97.55 35.2003 103.83L17.9704 134.32C7.09035 120.23 0.610352 102.58 0.610352 83.44C0.610352 37.43 38.0404 0 84.0504 0C130.06 0 167.49 37.43 167.49 83.44Z"
+        fill={primaryColor}
+      />
+      {/* <path
+        d="M232.8 151.06L158.73 19.9501C143.93 -6.24995 106.19 -6.24995 91.38 19.9501L17.31 151.06L0 181.7H76.89C87.65 181.7 97.92 177.22 105.24 169.33L122.19 151.06L125.06 147.97L127.92 151.06L156.35 181.7H250.11L232.8 151.06ZM131.19 33.5001L153.25 72.54L125.06 102.93L96.86 72.54L118.91 33.5101C121.61 28.7301 128.5 28.7301 131.2 33.5101L131.19 33.5001ZM80.4 151.06H52.5L81.04 100.54L104.16 125.45L80.4 151.06ZM169.71 151.06L145.95 125.45L169.07 100.54L197.61 151.06H169.71Z"
+        fill={secondaryColor}
+      /> */}
+    </SvgIcon>
+);
+
+export default function MapPage() {
+    const [places,setPlaces] = useState([])
+    const [bookmarks,setBookmarks] = useState([])
+    const [radius,setRadius] = useState(100)
+    const [search, setSearch] =  useState("")
+    const [openedTab,setTab] = useState(0)
+
+    function handleSearch(value){
+        setSearch(value);
+    }
+
     return (
-        <MapContainer
-            style={{ height: '100vh' }}
-			center={[0,0]}
-			zoom={17}
-			minZoom={3}
-			scrollWheelZoom={true}
-		>
-            <Map/>
-        </MapContainer>
+        <div>
+            <div className='overlay'>
+                <Paper elevation={3} className="app-bar">
+                    <div className='columned-container'>
+                        <LogoIcon primaryColor="primary" secondaryColor="secondary" />
+                        <Button variant={openedTab !=1 ? 'outlined' : "contained"} color="primary" onClick={()=>setTab(1)}>
+                            <SearchIcon color={openedTab !=1 ? "primary" : "white"}/>
+                        </Button>
+                        <Button variant={openedTab !=2 ? 'outlined' : "contained"} color="secondary" onClick={()=>setTab(2)}>
+                            <BookmarkIcon color={openedTab !=2 ? "secondary" : "white"}/>
+                        </Button>
+                    </div>
+                    <Link to = "/login">
+                        <IconButton>
+                            <LoginIcon/>
+                        </IconButton>
+                    </Link>
+                </Paper>
+                {openedTab == 1 && 
+                <Paper elevation={3} className="tab">
+                    <div className='flex-container outlined'>
+                        <SearchIcon/>
+                        <InputBase
+                        value={search}
+                        onChange={(e)=>handleSearch(e.target.value)}
+                        placeholder="place,adress…"
+                        inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
+                    <Typography variant="h6">
+                        Search:
+                    </Typography>
+                    <div className='categories outlined scroll-list'>
+                        {["sfsfsfs","ssgdfsd","eterte"].map((category)=>
+                            <Typography>
+                                {category}
+                            </Typography>
+                        )}
+                    </div>
+                    <Typography variant="h6">
+                        Radius
+                    </Typography>
+                    <div className='flex-container' style={{gap: '5px', width:"50%"}}>
+                        <InputBase className="outlined"
+                        value={radius}
+                        type="number"
+                        onChange={(e)=>setRadius(e.target.value)}
+                        inputProps={{ 'aria-label': 'radius' }}
+                        />
+                        km
+                    </div>
+                    <Button style={{alignSelf: 'flex-end', marginTop: "auto"}} variant="contained" fullWidth>
+                        <SearchIcon/>
+                    </Button>
+                </Paper>
+                }
+                {openedTab == 2 && 
+                <Paper elevation={3} className="tab">
+                    <div className='flex-container outlined'>
+                        <SearchIcon/>
+                        <InputBase
+                        value={search}
+                        onChange={(e)=>handleSearch(e.target.value)}
+                        placeholder="place,adress…"
+                        inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
+                    <div className='places-list scroll-list'>
+                        {bookmarks.map((place)=>
+                            <Card>
+                                
+                            </Card>
+                        )}
+                    </div>
+                </Paper>
+                }
+                {openedTab != 0 && <Paper elevation={3} style={{borderRadius: '0 8px 8px 0'}}>
+                    <IconButton onClick={()=>setTab(0)}>
+                        <CloseIcon/>
+                    </IconButton>
+                </Paper>}
+            </div>
+            <div className='map-container'>
+                <MapContainer
+                    style={{ height: '100vh' }}
+                    center={[0,0]}
+                    zoom={17}
+                    minZoom={3}
+                    zoomControl={false}
+                    scrollWheelZoom={true}
+                >
+                    <Map places={places} setPlaces={setPlaces} radius={radius}/>
+                </MapContainer>
+            </div>
+        </div>
     );
 };
-
-export default MapComponent;
